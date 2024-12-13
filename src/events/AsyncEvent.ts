6e10708dfefc8e98e5506eb7394e70663296e404
Vilmos Takacs <tvili999@gmail.com>
2024-12-13 01:58:59 +0100

    Initial commit
     - Added SyncEvent and AsyncEvent

---
+++ src/events/AsyncEvent.ts
@@ -0,0 +1,25 @@
+export type AsyncListener<T extends unknown[]> = (...eventArgs: T) => void | Promise<void>;
+
+export class AsyncEvent<T extends unknown[]> {
+  private listeners: Array<AsyncListener<T>> = [];
+
+  addEventListener(listener: AsyncListener<T>) {
+    this.listeners.push(listener);
+  }
+
+  removeEventListener(listener: AsyncListener<T>) {
+    const index = this.listeners.indexOf(listener);
+
+    if (index === -1) {
+      return;
+    }
+
+    this.listeners.splice(index, 1);
+  }
+
+  async fire(...eventArgs: T) {
+    for (const listener of this.listeners) {
+      await listener(...eventArgs);
+    }
+  }
+}
