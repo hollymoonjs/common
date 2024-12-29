b2d773928b87de173bba96bed1049026d886cf9d
Vilmos Takacs <tvili999@gmail.com>
2024-12-29 19:28:10 +0100

    configuring prettier

--- src/events/AsyncEvent.ts
+++ src/events/AsyncEvent.ts
@@ -1,25 +1,27 @@
-export type AsyncListener<T extends unknown[]> = (...eventArgs: T) => void | Promise<void>;
+export type AsyncListener<T extends unknown[]> = (
+    ...eventArgs: T
+) => void | Promise<void>;
 
 export class AsyncEvent<T extends unknown[]> {
-  private listeners: Array<AsyncListener<T>> = [];
+    private listeners: Array<AsyncListener<T>> = [];
 
-  addEventListener(listener: AsyncListener<T>) {
-    this.listeners.push(listener);
-  }
-
-  removeEventListener(listener: AsyncListener<T>) {
-    const index = this.listeners.indexOf(listener);
-
-    if (index === -1) {
-      return;
+    addEventListener(listener: AsyncListener<T>) {
+        this.listeners.push(listener);
     }
 
-    this.listeners.splice(index, 1);
-  }
+    removeEventListener(listener: AsyncListener<T>) {
+        const index = this.listeners.indexOf(listener);
 
-  async fire(...eventArgs: T) {
-    for (const listener of this.listeners) {
-      await listener(...eventArgs);
+        if (index === -1) {
+            return;
+        }
+
+        this.listeners.splice(index, 1);
     }
-  }
+
+    async fire(...eventArgs: T) {
+        for (const listener of this.listeners) {
+            await listener(...eventArgs);
+        }
+    }
 }
--- src/events/SyncEvent.ts
+++ src/events/SyncEvent.ts
@@ -1,25 +1,25 @@
 export type SyncListener<T extends unknown[]> = (...eventArgs: T) => void;
 
 export class SyncEvent<T extends unknown[]> {
-  private listeners: Array<SyncListener<T>> = [];
+    private listeners: Array<SyncListener<T>> = [];
 
-  addEventListener(listener: SyncListener<T>) {
-    this.listeners.push(listener);
-  }
-
-  removeEventListener(listener: SyncListener<T>) {
-    const index = this.listeners.indexOf(listener);
-
-    if (index === -1) {
-      return;
+    addEventListener(listener: SyncListener<T>) {
+        this.listeners.push(listener);
     }
 
-    this.listeners.splice(index, 1);
-  }
+    removeEventListener(listener: SyncListener<T>) {
+        const index = this.listeners.indexOf(listener);
 
-  fire(...eventArgs: T) {
-    for (const listener of this.listeners) {
-      listener(...eventArgs);
+        if (index === -1) {
+            return;
+        }
+
+        this.listeners.splice(index, 1);
     }
-  }
+
+    fire(...eventArgs: T) {
+        for (const listener of this.listeners) {
+            listener(...eventArgs);
+        }
+    }
 }
